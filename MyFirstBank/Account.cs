0da5af2c3c107fb7ab40dabc1ddd8ab2bbbe46c6
gpadmapriya <gpadmapriya@hotmail.com>
2017-01-28 11:40:49 -0800

    Built a menu

---
+++ MyFirstBank/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBank
+{
+    /// <summary>
+    /// This class defines an account for MyFirstBank
+    /// This is a blueprint
+    /// </summary>
+    class Account
+    {
+        #region Statics
+        private static int LastAccountNumber = 0;
+        #endregion
+        
+        #region Properties
+        public int AccountNumber { get; private set; }
+        public string EmailAddress { get; set; }
+
+        //if balance is public program.cs can set it which is not desirable
+        //if balance is set to private program.cs cannot even read balance to display
+        //so set balance to public but the set method to private
+        //visual c# how to program for beginners
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Methods
+        public Account():this("", 0.0M)
+        {
+            //this.AccountNumber = ++LastAccountNumber;
+        }
+        public Account(string EmailAddress):this(EmailAddress, 0.0M)
+        {
+            //this.AccountNumber = ++LastAccountNumber;
+            //this.EmailAddress = EmailAddress;
+        }
+        public Account(string EmailAddress, decimal balance)
+        {
+            this.AccountNumber = ++LastAccountNumber;
+            this.EmailAddress = EmailAddress;
+            this.Balance = balance;
+        }
+
+        public decimal Deposit(decimal Amount)
+        {
+            Balance += Amount;
+            return Balance;
+        }
+        public void WithDraw(decimal Amount)
+        {
+            Balance -= Amount;
+        }
+        #endregion
+    }
+}
---
+++ MyFirstBank/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("*****Welcome to my bank!****");
+            Console.WriteLine("0. Exit");
+            Console.WriteLine("1. Create an account");
+            Console.WriteLine("2. Deposit");
+            Console.WriteLine("3. Withdraw");
+            while (true)
+            {
+                Console.WriteLine("Please select one of the options from above");
+                var option = Console.ReadLine();
+
+                switch (option)
+                {
+                    case "0":
+                        return;
+
+                    case "1":
+                        Console.WriteLine("Please provide your email address: ");
+                        var email = Console.ReadLine();
+                        var newAccount = new Account(email);
+                        Console.WriteLine($"The balance in my account {newAccount.AccountNumber} is {newAccount.Balance:C}");
+                        Console.ReadLine();
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    default:
+                        Console.WriteLine("Sorry, option not available");
+                        break;
+
+                }
+            }
+
+            //account is created in the stack. Account() is in the heap(reference type)
+            /*var account = new Account();
+            //account.AccountNumber = 123;
+            account.EmailAddress = "test@test.com";
+            decimal bal = account.Balance;
+            //M - 3 different kinds of floating point variable-double, float, decimal.
+            //decimal is used for money M.
+            var newBalance = account.Deposit(3560.45M);
+            //$-telling c# that you are putting placeholders within the string
+            //C- balance is written in currency format
+            Console.WriteLine($"The balance in my account {account.AccountNumber} is {account.Balance:C}");
+
+            var account2 = new Account();
+            account2.EmailAddress = "test2@test.com";
+            newBalance = account2.Deposit(200.5M);
+            Console.WriteLine($"The balance in my account {account2.AccountNumber} is {account2.Balance:C}");
+
+            var account3 = new Account("test3@test.com");
+            newBalance = account3.Deposit(200.5M);
+            Console.WriteLine($"The balance in my account {account3.AccountNumber} is {account3.Balance:C}");
+
+            Console.ReadLine();
+            */
+        }
+    }
+}
